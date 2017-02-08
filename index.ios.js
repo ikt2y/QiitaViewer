e546cd4da5eb5c51b1a6cba34f75959a3dc9cb32
sel <ikd618@gmail.com>
2017-02-08 17:06:41 +0900

    arranged file

--- index.ios.js
+++ index.ios.js
@@ -3,33 +3,27 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
-export default class QiitaViewer extends Component {
-  render() {
+var QiitaViewer = React.createClass({
+    myFunction: function() {
+
+    },
+    render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={styles.instruction}>
         </Text>
       </View>
     );
-  }
-}
+    }
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -38,16 +32,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  instruction: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('QiitaViewer', () => QiitaViewer);
