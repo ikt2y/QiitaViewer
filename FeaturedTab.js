28371ed2dc84ebdd616ae904c037e09f7fc78226
sel <ikd618@gmail.com>
2017-02-14 15:53:38 +0900

    add import NavigatorIOS to featuredtab

--- FeaturedTab.js
+++ FeaturedTab.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 import EntryList from './EntryList.js';
 import {
   StyleSheet,
-  Text,
-  View,
+  NavigatorIOS,
 } from 'react-native';
 
 var FeaturedTab = React.createClass({
