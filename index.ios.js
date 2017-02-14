80c26c2a93a1854e6b11b5fcfdefbb356a9f0daa
sel <ikd618@gmail.com>
2017-02-14 14:14:11 +0900

    TabBarIOSを用いて切り替えロジックのベースを作成

--- index.ios.js
+++ index.ios.js
@@ -16,12 +16,64 @@ import {
 
 // コンポーネント
 var QiitaViewer = React.createClass({
+    // インスタンスが生成された時に呼ばれる
+        // 初期画面はFeaturedTabに固定
+    getInitialState: function() {
+        return(
+            {
+                selectedTab: 'FeaturedTab'
+            }
+        );
+    },
+    render: function() {
+        return(
+            <TabBarIOS selectedTab={this.state.selectedTab}>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'FeaturedTab'}
+                    icon={{uri: 'featured'}}
+                    onPress={() => {
+                        this.setState(
+                            {selectedTab: 'FeaturedTab'}
+                        );
+                    }}
+                >
+                    <FeaturedTab />
+                </TabBarIOS.Item>
+                <TabBarIOS.Item
+                    selected={this.state.selectedTab === 'SearchTab'}
+                    icon={{uri: 'search'}}
+                    onPress={() => {
+                        this.setState(
+                            {selectedTab: 'SearchTab'}
+                        );
+                    }}
+                >
+                    <SearchTab />
+                </TabBarIOS.Item>
+            </TabBarIOS>
+        )
+    }
 
 });
 
 // スタイル
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+      },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('QiitaViewer', () => QiitaViewer);
